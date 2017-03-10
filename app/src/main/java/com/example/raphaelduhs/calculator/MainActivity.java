fc79bbf648d313b0ae13b04b016fdcc224017ea9
raphaelduhs <raphael.duhs@gmail.com>
2017-03-10 21:20:03 +0100

    Update - almost finished

--- app/src/main/java/com/example/raphaelduhs/calculator/MainActivity.java
+++ app/src/main/java/com/example/raphaelduhs/calculator/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.raphaelduhs.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void calculate (View view) {
+
+        EditText val1 = (EditText) findViewById(R.id.valueOne);
+        EditText val2 = (EditText) findViewById(R.id.valueTwo);
+
+        TextView result = (TextView) findViewById(R.id.result);
+
+       double numb1 = Double.parseDouble(val1.getText().toString());
+       double numb2 = Double.parseDouble(val2.getText().toString());
+
+        double solution = numb1 / numb2;
+
+        result.setText("Resultat = "+ solution);
+
+
+
     }
 }
